3c211c52f17555421f707b576d625a8bb36500c3
fjh1997 <549308442@qq.com>
2019-09-16 13:55:06 +0800

    Add files via upload

---
+++ 斐讯老毛子备份/reconnect.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "开始检测"
+ping -c 4 1.1.1.1 
+	if [ "$?" != "0" ];then
+		ps w | grep l2tpd | grep -v grep | awk '{print $1}' | xargs kill -9
+		restart_wan
+		echo "已重连 " 
+		echo "ok">>/tmp/vpncon.log
+		if [ ! -f "/tmp/1.log" ] #五次重连不上则重启
+		then
+		touch "/tmp/1.log"
+		a=0
+		else
+		a=`cat /tmp/1.log`
+		fi
+		if [ $a -le 5 ]
+		then
+		echo $(($a+1))> 1.log
+		else
+		reboot
+		fi
+	fi  
+exit 0
